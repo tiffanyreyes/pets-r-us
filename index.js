4599bee875b96bdce427fe9bfca8cf8844ce71b3
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-04-23 23:10:09 -0400

    adding assignment 6.2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 
+const Customer = require('./models/customer')
 const app = express(); 
 
 
@@ -8,8 +10,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const CONN = 'mongodb+srv://web340_admin:7654@bellevueuniversity.paicaia.mongodb.net/web340DB'
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONN).then(() => {
+    console.log('Connection to MongoDB database was successful\n  If you see this message it means you were able to connect to your MongoDB Atlas cluster');
+}).catch(err => {
+    console.log('MongoDB Error: ' + err.message);
+})
 
 
 app.get('/', (req, res) => {
@@ -34,6 +46,31 @@ app.get('/training', (req, res) => {
     res.render('training', {
         title: 'Pets-R-Us',
     })
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Pets-R-Us',
+    })
+});
+
+app.post('/customers', (req, res, next) => {
+    const newCustomer = new Customer({
+        customerId: req.body.customerId, 
+        customerFirstName: req.body.customerFirstName,
+        customerLastName: req.body.customerLastName,
+        email: req.body.email
+    });
+
+    Customer.create(newCustomer)
+        .then((result) => {
+            res.render('index', {
+                title: 'Pets-R-Us'
+            })
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 app.listen(PORT, () => {});
